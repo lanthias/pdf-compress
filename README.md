23e01813568296bca9b502612d09e1e9eecf748c
script-deployer <script-deployer@me.com>
2015-07-27 18:02:44 +0100

    PDF compress second iteration, using ebook instead of /screen

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Pdf-compress Service
 
-_TODO_ - describe service
+Given the URL of a PDF, I will compress it and send it back to you. I do this by using ghostscript as follows:
+
+`gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=out.pdf in.pdf`
 
 ## Usage
 * Look at the API (defined in `api.idl`) to see what functions are available, friend.
--- app.py
+++ app.py
@@ -4,13 +4,20 @@
 {{ scaffold.name | capitalize }} Service
 """
 import stackhut
+import sh
 
 class DefaultService:
     def __init__(self):
         pass
 
-    def add(self, x, y):
-        return x + y
+    def compress(self, pdfUrl) :
+    	in_file = stackhut.download_file(pdfUrl)
+    	out_file = "out_{}".format(in_file)
+
+    	output = sh.gs("-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4", "-dPDFSETTINGS=/ebook", "-dNOPAUSE", "-dQUIET", "-dBATCH", "-sOutputFile={}".format(out_file), in_file)
+    	
+    	print(output)
+    	return stackhut.put_file(out_file)
 
 # export the services
 SERVICES = {"Default": DefaultService()}
